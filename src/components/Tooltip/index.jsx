aad34893b10f99c17d7dae95a0ad1fe84c458dc5
John <john.tan-aristy@csedge.org>
2026-06-14 12:38:44 -0600

    adding current stuff

--- src/components/Tooltip/index.jsx
+++ src/components/Tooltip/index.jsx
@@ -12,30 +12,30 @@ export default function ToolTip({ children, tooltip }) {
   };
 
   return (
-    <div
+    <span
       onMouseEnter={() => setMouseOver(true)}
       onMouseLeave={() => setMouseOver(false)}
       onMouseMove={handleMouseMove}
-      onFocus={() => setMouseOver(true)}  // For keyboard users
-      onBlur={() => setMouseOver(false)}  // For keyboard users
-      className="relative"
-      aria-describedby={tooltipId} // Describes the tooltip content for screen readers
+      onFocus={() => setMouseOver(true)}
+      onBlur={() => setMouseOver(false)}
+      className="relative cursor-default"
+      aria-describedby={tooltipId}
     >
       {children}
       {mouseOver && (
         <span
-          id={tooltipId}  // Links the tooltip with the element using aria-describedby
-          role="tooltip"  // Identifies the element as a tooltip
+          id={tooltipId}
+          role="tooltip"
           style={{
-            top: mousePosition.y + 10, 
-            left: mousePosition.x + 10, 
+            top: mousePosition.y + 10,
+            left: mousePosition.x + 10,
           }}
           className="z-50 fixed pointer-events-none bg-off-black px-2 py-1 text-off-white"
-          aria-live="polite"  // Ensures the tooltip is announced by screen readers when it appears
+          aria-live="polite"
         >
           {tooltip}
         </span>
       )}
-    </div>
+    </span>
   );
 }
